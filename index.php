33b325c91ac964939c194beb691841b4dbce57a2
Jester101222 <wolfunder8@gmail.com>
2024-05-16 18:15:41 +0800

    My first Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('includes/header.php'); ?>
+
+<div>
+    <div class="container">
+        <div class="">
+            <div class="col-md-12 text-secondary d-flex flex-column align-items-center justify-content-center"> 
+                    <img class="w-50" src="admin/assets/img/pet-shop-logo-design.jpg" alt=""></div>
+            </div>
+    </div>
+</div>
+<?php include('includes/footer.php') ?>
